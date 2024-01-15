0b9eeda0dca0d3747eb78cc4d17f115b81902eee
Károlyi Krisztián <145534392+krisztiankarolyi@users.noreply.github.com>
2024-01-15 20:07:53 +0200

    ContextAndOptionsMenu.java

---
+++ ContextAndOptionsMenu, .java
@@ -0,0 +1,35 @@
+   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        registerForContextMenu(listView);
+    
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        if (item.getItemId() == R.id.valami) {
+         //toDo
+        }
+    }
+
+//-------------------------------------------------
+
+  public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      if (item.getItemId() == R.id.valami) {
+         //toDo
+        }
+  }
